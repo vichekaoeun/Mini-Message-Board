d4728ae8b4e4f4695fc3a43830c6f59b8c9a3ab9
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-02-02 04:09:32 -0500

    setup routes, GET requests, POST requests

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: "Mini Messageboard", messages: messages });
 });
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'Form' });
+});
+router.post('/new', function (req, res, next) {
+  const messageUser = req.body.name;
+  const messageText = req.body.text;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
+})
 
 module.exports = router;
